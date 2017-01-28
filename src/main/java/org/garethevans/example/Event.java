05dd5df7ee04c0b3a74ef1cd460282c60d2184d8
Nur Rohman <ennurr31@gmail.com>
2017-01-29 02:32:03 +0700

    update

---
+++ src/main/java/org/garethevans/example/Event.java
@@ -0,0 +1,16 @@
+package org.garethevans.example;
+
+import java.util.List;
+
+public class Event {
+
+    private Boolean success;
+    private List<Data> data;
+    public Boolean getSuccess ()
+    {
+        return success;
+    }
+    public List<Data> getData(){
+        return data;
+    }
+}
