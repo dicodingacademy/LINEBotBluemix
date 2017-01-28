926bd6e78a0f35e3176ae5c04856cb498db03d4b
Nur Rohman <ennurr31@gmail.com>
2017-01-29 03:37:52 +0700

    update

--- src/main/java/org/garethevans/example/Message.java
+++
@@ -1,7 +0,0 @@
-package org.garethevans.example;
-
-public class Message {
-    public String type;
-    public String id;
-    public String text;
-}
--- src/main/java/org/garethevans/example/Source.java
+++
@@ -1,8 +0,0 @@
-package org.garethevans.example;
-
-public class Source {
-    public String userId;
-    public String groupId;
-    public String roomId;
-    public String type;
-}
