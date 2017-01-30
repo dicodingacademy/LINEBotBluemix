39e21605725e1a49b5e63f59c649d3d32318c325
Nur Rohman <ennurr31@gmail.com>
2017-01-30 20:48:13 +0700

    update

--- src/main/java/org/garethevans/example/controller/LineBotController.java
+++ src/main/java/org/garethevans/example/controller/LineBotController.java
@@ -13,6 +13,11 @@ import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.model.message.template.CarouselColumn;
 import com.linecorp.bot.model.message.template.CarouselTemplate;
 import com.linecorp.bot.model.response.BotApiResponse;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.garethevans.example.Event;
 import org.garethevans.example.Payload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -21,8 +26,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import retrofit2.Response;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 @RestController
 @RequestMapping(value="/linebot")
@@ -35,6 +44,20 @@ public class LineBotController
     @Autowired
     @Qualifier("com.linecorp.channel_access_token")
     String lChannelAccessToken;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    @ResponseBody
+    public String hello() {
+        return "hello world";
+    }
+
+    @RequestMapping(value = "/line", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    @ResponseBody
+    public String line() {
+        return lChannelAccessToken;
+    }
 
     @RequestMapping(value="/callback", method=RequestMethod.POST)
     public ResponseEntity<String> callback(
@@ -129,73 +152,72 @@ public class LineBotController
 //        System.out.println("Text from User: " + title);
 
         // Act as client with GET method
-//        String URI = "https://www.dicoding.com/public/api/events";
-//        System.out.println("URI: " +  URI);
-//
-//        String jObjGet = " ";
-//        CloseableHttpAsyncClient c = HttpAsyncClients.createDefault();
-//
-//        try{
-//            c.start();
-//            //Use HTTP Get to retrieve data
-//            HttpGet get = new HttpGet(URI);
-//
-//            Future<HttpResponse> future = c.execute(get, null);
-//            HttpResponse responseGet = future.get();
-//            System.out.println("HTTP executed");
-//            System.out.println("HTTP Status of response: " + responseGet.getStatusLine().getStatusCode());
-//
-//            // Get the response from the GET request
-//            BufferedReader brd = new BufferedReader(new InputStreamReader(responseGet.getEntity().getContent()));
-//
-//            StringBuffer resultGet = new StringBuffer();
-//            String lineGet = "";
-//            while ((lineGet = brd.readLine()) != null) {
-//                resultGet.append(lineGet);
-//            }
-//            System.out.println("Got result");
-//
-//            // Change type of resultGet to JSONObject
-//            jObjGet = resultGet.toString();
-//            System.out.println("OMDb responses: " + jObjGet);
-//        } catch (InterruptedException | ExecutionException e) {
-//            System.out.println("Exception is raised ");
-//            e.printStackTrace();
-//        } finally {
-//            c.close();
-//        }
-//
-//        Gson mGson = new Gson();
-//        Event event = mGson.fromJson(jObjGet, Event.class);
-//        String owner = event.getData().get(0).getOwner_display_name();
-//        String summary = event.getData().get(0).getSummary();
-//        String description = event.getData().get(0).getDescription();
-//        String link = event.getData().get(0).getLink();
-//        String time = event.getData().get(0).getBegin_time() + " - " + event.getData().get(0).getEnd_time();
-//        String address = event.getData().get(0).getAddress();
-//        String image = event.getData().get(0).getImage_path();
-//        String msgToUser = " ";
+        String URI = "https://www.dicoding.com/public/api/events";
+        System.out.println("URI: " +  URI);
+
+        String jObjGet = " ";
+        CloseableHttpAsyncClient c = HttpAsyncClients.createDefault();
+
+        try{
+            c.start();
+            //Use HTTP Get to retrieve data
+            HttpGet get = new HttpGet(URI);
+
+            Future<HttpResponse> future = c.execute(get, null);
+            HttpResponse responseGet = future.get();
+            System.out.println("HTTP executed");
+            System.out.println("HTTP Status of response: " + responseGet.getStatusLine().getStatusCode());
+
+            // Get the response from the GET request
+            BufferedReader brd = new BufferedReader(new InputStreamReader(responseGet.getEntity().getContent()));
+
+            StringBuffer resultGet = new StringBuffer();
+            String lineGet = "";
+            while ((lineGet = brd.readLine()) != null) {
+                resultGet.append(lineGet);
+            }
+            System.out.println("Got result");
+
+            // Change type of resultGet to JSONObject
+            jObjGet = resultGet.toString();
+            System.out.println("OMDb responses: " + jObjGet);
+        } catch (InterruptedException | ExecutionException e) {
+            System.out.println("Exception is raised ");
+            e.printStackTrace();
+        } finally {
+            c.close();
+        }
+
+        Gson mGson = new Gson();
+        Event event = mGson.fromJson(jObjGet, Event.class);
+        String owner = event.getData().get(0).getOwner_display_name();
+        String summary = event.getData().get(0).getSummary();
+        String description = event.getData().get(0).getDescription();
+        String link = event.getData().get(0).getLink();
+        String time = event.getData().get(0).getBegin_time() + " - " + event.getData().get(0).getEnd_time();
+        String address = event.getData().get(0).getAddress();
+        String image = event.getData().get(0).getImage_path();
+        String msgToUser = " ";
 
         //Check user's request
         if (userTxt.contains("name")){
-            pushMessage(targetID, "name");
+            pushMessage(targetID, event.getData().get(0).getName());
+        } else if (userTxt.contains("summary")){
+            pushMessage(targetID, summary);
+        } else if (userTxt.contains("description")){
+            pushMessage(targetID, description);
+        } else if (userTxt.contains("time")){
+            pushMessage(targetID, "time");
+        } else if (userTxt.contains("address")){
+            pushMessage(targetID, address);
+        } else if (userTxt.contains("owner")){
+            pushMessage(targetID, owner);
         }
-//        else if (userTxt.contains("summary")){
-//            pushMessage(targetID, summary);
-//        } else if (userTxt.contains("description")){
-//            pushMessage(targetID, description);
-//        } else if (userTxt.contains("time")){
-//            pushMessage(targetID, "time");
-//        } else if (userTxt.contains("address")){
-//            pushMessage(targetID, address);
-//        } else if (userTxt.contains("owner")){
-//            pushMessage(targetID, owner);
-//        }
-//        else if (userTxt.contains("event")){
-//            carouselForUser(image, ePayload.events[0].source.userId, owner, link);
-//        }
-//
-//        System.out.println("Message to user: " + image);
+        else if (userTxt.contains("event")){
+            carouselForUser(image, ePayload.events[0].source.userId, owner, link);
+        }
+
+        System.out.println("Message to user: " + image);
 
 //        //Check whether response successfully retrieve or not
 //        if (msgToUser.length() <= 11 || !ePayload.events[0].message.type.equals("text")){
